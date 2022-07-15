7686859d93c1bd094b781b023fc1f9835a423d3f
jennellelai907 <Jennellelai907@users.noreply.github.com>
2022-07-15 14:58:45 +0800

    upload

--- Analyzing TV Data/Python/Task_03.py
+++ Analyzing TV Data/Python/Task_03.py
@@ -1,6 +1,10 @@
 from matplotlib import pyplot as plt
 plt.style.use('seaborn')
+
 plt.hist(super_bowls.combined_pts)
 plt.xlabel('Combined Points')
 plt.ylabel('Number of Super Bowls')
 plt.show()
+
+display(super_bowls[super_bowls['combined_pts'] > 70])
+display(super_bowls[super_bowls['combined_pts'] < 25])
